0e79a2ac76c19e624d61daeb839d4b138e7e4dfd
arianeitetero <a.itetero@alustudent.com>
2025-11-25 14:34:54 +0200

    fixed documentation

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,33 +1,17 @@
 #!/usr/bin/python3
-"""
-Module: 1-top_ten
-Queries the Reddit API and prints the titles of the first 10 hot posts
-for a given subreddit. Prints 'OK' if the subreddit is invalid or request fails.
-"""
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the first 10 hot post titles or 'OK' if invalid subreddit."""
-    URL = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    HEADERS = {"User-Agent": "python:topten:v1.0"}
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
     try:
         RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        if RESPONSE.status_code != 200:
-            print("OK")
-            return
-
-        HOT_POSTS = RESPONSE.json().get("data", {}).get("children")
-        if not HOT_POSTS:
-            print("OK")
-            return
-
-        for post in HOT_POSTS[:10]:
-            title = post.get("data", {}).get("title")
-            if title:
-                print(title)
-
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
     except Exception:
-        print("OK")
+        print(None)
